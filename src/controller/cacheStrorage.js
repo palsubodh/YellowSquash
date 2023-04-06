88bf83a5a036242deac71e92becc9a78b3eb52ba
palsubodh <palsubodh390@gmail.com>
2023-04-06 22:25:35 +0530

    auth api done

--- src/controller/cacheStrorage.js
+++ src/controller/cacheStrorage.js
@@ -59,7 +59,7 @@ const forgetOTP =  async (req,res)=>{
         encoding: 'base32'
       });
 
-      
+
     var options = { method: 'GET',
     url: `https://api.authkey.io/request?authkey=651ef6692f009509&mobile=${phone}&country_code=91&sid=7919&company=YellowSquash&otp=${token}`,
     }
@@ -103,56 +103,3 @@ module.exports ={phoneNumberStore,verifyOTP,forgetOTP,forgetverifyOTP}
 
 
 
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const speakeasy = require('speakeasy');
-
-// const app = express();
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // Route to generate an OTP
-// app.post('/otp', (req, res) => {
-//   const secret = speakeasy.generateSecret();
-//   const token = speakeasy.totp({
-//     secret: secret.base32,
-//     encoding: 'base32',
-//     digits: 6,
-//     step: 300 // OTP expires after 5 minutes (300 seconds)
-//   });
-
-//   // Send the secret and OTP to the client
-//   res.json({
-//     secret: secret.base32,
-//     token: token
-//   });
-// });
-
-// // Route to verify an OTP
-// app.post('/verify-otp', (req, res) => {
-//   const token = req.body.token;
-//   const secret = req.body.secret;
-
-//   // Verify the OTP
-//   const verified = speakeasy.totp.verify({
-//     secret: secret,
-//     encoding: 'base32',
-//     token: token,
-//     window: 1 // Allow 1 time step (5 minutes) for time skew
-//   });
-
-//   if (verified) {
-//     console.log(`OTP verification success for ${secret}`);
-//     res.send('OTP verified successfully');
-//   } else {
-//     console.log(`OTP verification failed for ${secret}`);
-//     res.status(401).send('Invalid OTP');
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
-
-//db.otps.createIndex({ createdAt: 1 }, { expireAfterSeconds: 20 });
